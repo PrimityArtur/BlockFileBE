ad850ce2b1cfa50a13b4aea8e50be3550be555d1
PrimityArtur <primityartur@gmail.com>
2025-09-21 06:21:13 -0500

    Agregado de models.py de la aplicacion users para administrador, usuario, cliente. Registrado tambien en admin.py,
    Agregado el DDL.sql script para ejecutar toda la base de datos de forma manual.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Usuario, Administrador, Cliente
 # Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id_usuario', 'nombre_usuario', 'correo')
+    search_fields = ('nombre_usuario', 'correo')
+
+@admin.register(Administrador)
+class AdministradorAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'acceso')
+    search_fields = ('usuario__nombre_usuario',)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'saldo', 'excliente', 'fecha_creacion')
+    list_filter = ('excliente',)
+    search_fields = ('usuario__nombre_usuario', 'usuario__correo')

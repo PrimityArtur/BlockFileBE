06802ceb6dfe279b0d445c6cd627e7bec78f08b6
PrimityArtur <primityartur@gmail.com>
2025-09-28 19:00:43 -0500

    cambios de login

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("iniciar/", views.iniciar_sesion_view, name="iniciar"),
     path("registrarse/", views.registrarse_view, name="registrarse"),
     path("catalogo/", views.catalogo_view, name="catalogo"),
-    path("perfil_admin/", views.perfil_admin_view, name="perfil_admin"),
+    path("perfil-admin/", views.perfil_admin_view, name="perfil_admin"),
+    path("perfil-usuario/", views.perfil_usuario_view, name="perfil_usuario"),
     path("logout/", views.logout_view, name="logout"),
     path("", views.iniciar_sesion_view),
 ]

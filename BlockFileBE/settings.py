51bc2905d0ad22f17e9e18244ebac7872bddd405
PrimityArtur <primityartur@gmail.com>
2025-10-19 13:59:32 -0500

    agregado whitenose

--- BlockFileBE/settings.py
+++ BlockFileBE/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django_filters',
     'corsheaders',
     'drf_spectacular',
-
+    'whitenoise.runserver_nostatic',
     #apps
     'core.apps.coreConfig',
     'users.apps.UsersConfig',
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,9 +149,15 @@ STATICFILES_DIRS = [
     # BASE_DIR / "static"
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, staticfiles)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn BlockFileBE.wsgi
+web: python manage.py collectstatic && gunicorn BlockFileBE.wsgi

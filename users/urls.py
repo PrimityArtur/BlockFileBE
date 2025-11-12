04aa32f42688c754abe8191e463b5bbc1bb571bd
PrimityArtur <primityartur@gmail.com>
2025-11-12 18:18:56 -0500

    boton iniciar cesion cambio de redireccion en inicio

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-
+from catalogoProductos import views as catProd
 app_name = 'users'
 
 urlpatterns = [
+    path("", catProd.catalogo_productos_view, name="iniciar"),
+
     path("iniciar/", views.iniciar_sesion_view, name="iniciar"),
     path("registrarse/", views.registrarse_view, name="registrarse"),
     path("perfil-admin/", views.perfil_admin_view, name="perfil_admin"),
     path("logout/", views.logout_view, name="logout"),
-    path("", views.iniciar_sesion_view),
 ]
